687bac8326707b23a82e4389ef61ff7b89402a2c
Herjus <herjus420@gmail.com>
2023-10-08 23:10:12 +0200

    egui start

---
+++ Egui/target/debug/Egui.d
@@ -0,0 +1,1 @@
+/Users/herjus/Repositories/Rust\ playground/Egui/target/debug/Egui: /Users/herjus/Repositories/Rust\ playground/Egui/src/main.rs
---
+++ Egui/target/debug/deps/Egui-bb4747dfa64202d1.d
@@ -0,0 +1,5 @@
+/Users/herjus/Repositories/Rust playground/Egui/target/debug/deps/Egui-bb4747dfa64202d1: src/main.rs
+
+/Users/herjus/Repositories/Rust playground/Egui/target/debug/deps/Egui-bb4747dfa64202d1.d: src/main.rs
+
+src/main.rs:
